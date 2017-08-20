c1aeccc672b11bfe797699366d82d2eb9d18df7f
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-20 19:52:17 +0000

    login reg working with errors

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  # skip_before_action :require_login, only: [:create]
+  def index
+    #code
+  end
+
+def create
+    user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+    if user
+        session[:user_id] = user.id
+        return redirect_to "/ideas"
+    else
+        flash[:errors] = ['To To Bad, So Sad. This Combo will not work']
+        return redirect_to :back
+    end
+end
+
+  def destroy
+      session[:user_id] = nil
+      return redirect_to '/'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ActiveRecord::Base
   validates :name, :alias, :email, presence: true
   validates :password, on: :create, length: { minimum: 8 }
   validates :email, uniqueness: true, format: { with: /(\A([a-z]*\s*)*\<*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\>*\Z)/i }
-  
-  before_save :downcase_email
 
+  before_save :downcase_input
+
+  def downcase_input
+    self.email.downcase!
+    self.name.downcase!
+    self.alias.downcase!
+  end
 end
